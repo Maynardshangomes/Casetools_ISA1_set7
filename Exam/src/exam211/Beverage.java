523eb1b2f310268212921c0e314c2fb0dc8cc823
Maynardshangomes <maynardgomes27@gmail.com>
2024-02-10 08:54:48 +0530

    Q1)

---
+++ Exam/src/exam211/Beverage.java
@@ -0,0 +1,18 @@
+package exam211;
+
+public class Beverage {
+	String type;
+	 Beverage (String type)
+	 {
+		 this.type=type;
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Beverage tea = new Beverage("Hot");
+		System.out.println("tea is"+tea.type);
+		
+
+	}
+
+}
